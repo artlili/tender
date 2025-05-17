a574bc8324b39ade690dbad9b907f05c5e86029b
liliagazadyan <agazadyan@gmail.com>
2025-05-17 15:18:22 +0300

    feat: add global scroll-to-top behavior in router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,16 +2,20 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import TenderDetailsView from '@/views/TenderDetailsView.vue'
 
+const routes = [
+  { path: '/', component: HomeView },
+  { path: '/tenders/:id', component: TenderDetailsView, name: 'tender-details' },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    { path: '/tenders/:id', component: TenderDetailsView, name: 'tender-details' },
-  ],
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    return { top: 0, behavior: 'smooth' }
+  },
 })
 
 export default router
