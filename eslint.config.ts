e80dafc0b0268d3c5a8313270121e6f4381d7724
liliagazadyan <agazadyan@gmail.com>
2025-05-17 19:36:26 +0300

    chore: update ESLint config and fix first-load fetch issue

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,17 +3,15 @@ import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescri
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
-export default [
-  defineConfigWithVueTs(
-    {
-      name: 'app/files-to-lint',
-      files: ['**/*.{ts,mts,tsx,vue}'],
-    },
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
 
-    globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
-    pluginVue.configs['flat/essential'],
-    vueTsConfigs.recommended,
-    skipFormatting,
-  ),
-]
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+  skipFormatting,
+)
